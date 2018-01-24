ce9d0d7dce78b422beca90204ef5ba9460020540
Mahogany Maddox <mahoganymaddox@Interapts-MacBook-Pro-76.local>
2018-01-24 13:23:24 -0500

    outlined project

---
+++ guessing-game/main.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  guessing-game
+//
+//  Created by Mahogany Maddox on 1/23/18.
+//  Copyright © 2018 Mahogany Maddox. All rights reserved.
+//
+
+import Foundation
+
+//Generate a random number
+var randomNumber = arc4random_uniform(100)
+print("guess a number 1...100")
+//Ask user input
+let response = readLine()
+//Take user input
+
+//Determine wrong or right(Loop unitl correct)
+
+//If correct ask if they want to play again
+print("good job, would you like to play again?")
+//if incorrect tell them the correct answer and ask them to play again
+
+
+
+
